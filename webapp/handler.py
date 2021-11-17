7e10d8ab02b9ae1b7f2c6f54b2a48362a02824ab
Lucas <lucasokamura@gmail.com>
2021-11-16 23:52:01 -0300

    Fixed gitignore final

---
+++ webapp/handler.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, Response
+import os
+import pandas as pd
+import pickle
+from rossmann.Rossmann import Rossmann
+
+# loading model
+with open('model/model_rossmann.pickle', 'rb') as file:
+    model = pickle.load( file )
+
+# initialize API
+app = Flask( __name__ )
+
+@app.route( '/rossmann/predict', methods=['POST'] )
+def rossmann_predict():
+    test_json = request.get_json()
+   
+    if test_json: # there is data
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+            
+        else: # multiple example
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+            
+        # Instantiate Rossmann class
+        pipeline = Rossmann()
+        
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+        
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+        
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
+        
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        
+        return df_response
+        
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run( host = "0.0.0.0", port = port )
